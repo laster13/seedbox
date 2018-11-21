1db0b76d20634f4b3afd55a6d938e877e91c525a
laster13 <patrick@lastergeek.com>
2018-11-21 20:24:28 +0100

    Update seedbox.sh

--- seedbox.sh
+++ seedbox.sh
@@ -64,11 +64,6 @@ echo -e "${CCYAN}INSTALLATION${CEND}"
 				apt install docker.io -y
                         	curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
                         	chmod +x /usr/local/bin/docker-compose
-				clear
-				logo.sh
-				echo -e "${CCYAN}Installation docker & docker compose terminée${CEND}"
-				echo ""
-				read -p "Appuyer sur la touche Entrer pour revenir au menu principal"
 				mkdir -p /etc/apache2
 				touch /etc/apache2/.htpasswd
 				clear
