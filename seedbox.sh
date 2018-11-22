01426ef83e0fda31a600d5f225ad45dbb8225816
laster13 <patrick@lastergeek.com>
2018-11-22 06:09:44 +0100

    Update seedbox.sh

--- seedbox.sh
+++ seedbox.sh
@@ -61,6 +61,7 @@ echo -e "${CCYAN}INSTALLATION${CEND}"
 				curl
 				unzip
 				apt install docker.io apache2-utils -y
+				curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
                         	curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
                         	chmod +x /usr/local/bin/docker-compose
 				mkdir -p /etc/apache2
